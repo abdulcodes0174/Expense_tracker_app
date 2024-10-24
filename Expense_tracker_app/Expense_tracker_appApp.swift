fe66b9b55b652224cf275b5002314cead443872c
abdulcodes0174 <abdulcodes0174@gmail.com>
2024-10-23 22:38:09 -0400

    Initial Commit

---
+++ Expense_tracker_app/Expense_tracker_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_tracker_appApp.swift
+//  Expense_tracker_app
+//
+//  Created by Abdul Malik on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_tracker_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Expense_tracker_appTests/Expense_tracker_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_tracker_appTests.swift
+//  Expense_tracker_appTests
+//
+//  Created by Abdul Malik on 2024-10-23.
+//
+
+import Testing
+@testable import Expense_tracker_app
+
+struct Expense_tracker_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
